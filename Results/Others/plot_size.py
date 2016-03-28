77e9d11ea237254cab9d98835b695ac0ef700a41
Wael Farhan <wael34218@gmail.com>
2016-03-28 10:50:36 -0700

    Adding extra plotting

---
+++ Results/Others/plot_size.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import csv
+
+selected_diags = ["d_584", "d_428", "d_416", "d_486", "d_493", "d_327", "d_038"]
+plt.figure(figsize=(12, 10), dpi=100)
+with open('skip_size.csv', 'rU') as f:
+    reader = csv.reader(f)
+    x = []
+    for line in reader:
+        if len(x) == 0:
+            x = line[2:]
+            continue
+
+        if line[0] in selected_diags:
+            plt.plot(x, line[2:], label=line[1], linewidth=2)
+
+plt.xlabel('Vector Size', fontsize=14, color='black')
+plt.ylabel('AUC', fontsize=14, color='black')
+plt.legend(framealpha=0.5, loc=4)
+plt.savefig('skip_size.png')
